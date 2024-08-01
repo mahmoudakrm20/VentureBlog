c66ef433cfc4e310e23f31a6323c46a740e9afef
mahmoudAkram <mahmoudakrm20@gmail.com>
2024-08-02 00:07:10 +0300

    Updates with React project

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -129,14 +129,14 @@ export default function Posts() {
   const nextPage = () => {
     if (currentPage < Math.ceil(posts.length / postsPerPage)) {
       setCurrentPage(currentPage + 1);
-      window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll to the top smoothly
+      window.scrollTo({ top: 0, behavior: "smooth" });
     }
   };
 
   const prevPage = () => {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
-      window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll to the top smoothly
+      window.scrollTo({ top: 0, behavior: "smooth" });
     }
   };
 
